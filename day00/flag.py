220fa35f7437cd32ee77f6c1942376bee6eaab08
rooho <roohom@qq.com>
2020-02-23 22:21:56 +0800

    20200223,locate in xuangang(051)

--- day00/flag.py
+++ day00/flag.py
@@ -52,16 +52,16 @@ def main():
     # 画大星星
     pice = 22
     center_x , center_y = x + 5*pice, y + height - 5*pice
-    turtle.goto(center_x,center_y)
+    turtle.goto(center_x, center_y)
     turtle.left(90)
     turtle.forward(pice*3)
     turtle.right(90)
-    draw_star(turtle.xcor(),turtle.ycor(), pice*3)
+    draw_star(turtle.xcor(), turtle.ycor(), pice*3)
     x_poses, y_poses = [10, 12, 12, 10], [2, 4, 7, 9]
     # 画小行星
     for x_pos, y_pos in zip(x_poses, y_poses):
         turtle.goto(x+ x_pos * pice, y + height - y_pos * pice)
-        turtle.left(turtle.towards(center_x,center_y) - turtle.heading())
+        turtle.left(turtle.towards(center_x, center_y) - turtle.heading())
         turtle.forward(pice)
         turtle.right(90)
         draw_star(turtle.xcor(), turtle.ycor(), pice)
