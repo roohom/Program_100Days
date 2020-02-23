19de17329a73658e5e5ac6a268e5a6a7502c7e93
rooho <roohom@qq.com>
2020-02-23 12:43:44 +0800

    始于20200223，为坚持100天每天打卡GitHub！

---
+++ day00/hello.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/2/23 11:19
+# @Author  : Roohom
+# @Site    : 
+# @File    : hello.py
+# @Software: PyCharm
+
+"""
+第一个python程序-Hello world!
+向伟大的程序员Dennis M.Ritchie先生致敬！
+
+Hello world!
+你好世界!
+"""
+
+print("Hello world!")
+print("你好，世界！")
+print("Goodbye world!")
+print("再见，世界！")
+print('Hello', 'World', sep=',', end='!')
+print('Goodbye, world', end='!\n')
