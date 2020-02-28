0d830d4602958e2b7a2310ad3c22a7f0c0aa84b1
rooho <roohom@qq.com>
2020-02-28 21:29:13 +0800

    20200228,locate in xuangang(051)

---
+++ day03/for1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/2/28 21:28
+# @Author  : Roohom
+# @Site    : 
+# @File    : for1.py
+# @Software: PyCharm
+
+sum = 0
+for x in range(101):
+    sum += x
+
+print(sum)
