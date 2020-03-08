4e6f525e07b9f70a894b31055d553e8703b950ea
rooho <roohom@qq.com>
2020-03-08 22:15:59 +0800

    20200304,locate in xuangang(051)

---
+++ day10/ex1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/3/8 22:07
+# @Author  : Roohom
+# @Site    : 
+# @File    : ex1.py
+# @Software: PyCharm
+
+"""
+异常机制-处理程序在运行时可能发生的状态
+"""
+
+input_again = True
+while input_again:
+    try:
+        a = int(input('a='))
+        b = int(input('b='))
+        print('%d / %d = %f' % (a, b, a/b))
+        input_again = False
+    except ValueError:
+        print("请输入整数")
+    except ZeroDivisionError:
+        print("除数不能为零")
+
---
+++ day10/file1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/3/8 22:15
+# @Author  : Roohom
+# @Site    : 
+# @File    : file1.py
+# @Software: PyCharm
+
